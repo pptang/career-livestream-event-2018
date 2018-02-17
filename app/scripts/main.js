831a140a72ac9387c244d6662319be85fcc09683
pptang <donaldduck518@gmail.com>
2018-02-17 10:53:32 +0900

    Add speaker popup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,11 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
   const $allArticle = $('#all_article');
   const $speakerArticle = $('#speaker_article');
   const $eventIntro = $('#event_intro');
   const $allSection = $('#all_section');
   const $eventSection = $('#event_section');
   const $speakerSection = $('#speaker_section');
-  $allArticle.click(function () {
+  $allArticle.click(function() {
     $allArticle.addClass('tab_selected');
     $speakerArticle.removeClass('tab_selected');
     $eventIntro.removeClass('tab_selected');
@@ -14,7 +13,7 @@ $(document).ready(function () {
     $eventSection.addClass('hidden');
     $speakerSection.addClass('hidden');
   });
-  $speakerArticle.click(function () {
+  $speakerArticle.click(function() {
     $allArticle.removeClass('tab_selected');
     $speakerArticle.addClass('tab_selected');
     $eventIntro.removeClass('tab_selected');
@@ -22,7 +21,7 @@ $(document).ready(function () {
     $eventSection.addClass('hidden');
     $speakerSection.removeClass('hidden');
   });
-  $eventIntro.click(function () {
+  $eventIntro.click(function() {
     $allArticle.removeClass('tab_selected');
     $speakerArticle.removeClass('tab_selected');
     $eventIntro.addClass('tab_selected');
@@ -30,7 +29,6 @@ $(document).ready(function () {
     $eventSection.removeClass('hidden');
     $speakerSection.addClass('hidden');
   });
-
 
   // $('.column-content__item').click(function () {
   //   var title = $(this).attr('data-title');
@@ -47,20 +45,42 @@ $(document).ready(function () {
   //     $('body').addClass('noscroll');
   //   }
   // });
-  // $('.member__list-item').click(function () {
-  //   var name = $(this).attr('data-name');
-  //   var photo = $(this).attr('data-photo');
-  //   var description = $(this).attr('data-description');
+  $('.speaker-content__item').click(function() {
+    $('#modal-speaker').css('display', 'block');
+    $('body').addClass('noscroll');
+    var contentTitle = $(this).attr('data-content-title');
+    var speakerTitle = $(this).attr('data-speaker-title');
+    var speakerPic = $(this).attr('data-speaker-pic');
+    var speakerIntro = $(this).attr('data-speaker-intro');
+    var profileName = $(this).attr('data-profile-name');
+    var profilePositionTitle = $(this).attr('data-profile-position-title');
+    var profileJpYear = $(this).attr('data-profile-jp-year');
+    var profileCareerSwitchTimes = $(this).attr('data-profile-career-switch-times');
+    var profileOverseaExperience = $(this).attr('data-profile-oversea-experience');
+    var profileJpFavorite = $(this).attr('data-profile-jp-favorite');
+    var profileLifeDescription = $(this).attr('data-profile-life-description');
+    var profileSuggestion = $(this).attr('data-profile-suggestion');
 
-  //   $('#modal-member .modal__member-name').text(name);
-  //   $('#modal-member .modal__member-photo').attr('src', photo);
-  //   $('#modal-member .modal__member-description').text(description);
+    $('#content-title').text(contentTitle);
+    $('#speaker-title').text(speakerTitle);
+    $('#speaker-pic').attr('src', speakerPic);
+    $('#speaker-intro').text(speakerIntro);
+    $('#profile-name').text(profileName);
+    $('#profile-position-title').text(profilePositionTitle);
+    $('#profile-jp-year').text(profileJpYear);
+    $('#profile-career-switch-times').text(profileCareerSwitchTimes);
+    $('#profile-oversea-experience').text(profileOverseaExperience);
+    $('#profile-jp-favorite').text(profileJpFavorite);
+    $('#profile-life-description').text(profileLifeDescription);
+    $('#profile-suggestion').text(profileSuggestion);
+  });
 
-  //   $('#modal-member').css('display', 'block');
-  //   $('body').addClass('noscroll');
-  // });
+  $('.modal__speaker-content__close-btn').click(function() {
+    $('#modal-speaker').css('display', 'none');
+    $('body').removeClass('noscroll');
+  });
 
-  $('#modal__close').click(function () {
+  $('#modal__close').click(function() {
     $('#modal-member').css('display', 'none');
     $('body').removeClass('noscroll');
   });
@@ -78,12 +98,12 @@ $(document).ready(function () {
   //   $('body').addClass('noscroll');
   // });
 
-  $('#modal-close').click(function () {
+  $('#modal-close').click(function() {
     $('#modal-column').css('display', 'none');
     $('body').removeClass('noscroll');
   });
 
-  $('.speaker__list-item').click(function () {
+  $('.speaker__list-item').click(function() {
     var video = $(this).attr('data-video');
 
     $('#modal-speaker iframe').attr('src', video);
@@ -91,13 +111,13 @@ $(document).ready(function () {
     $('body').addClass('noscroll');
   });
 
-  $('#modal-speaker-close').click(function () {
+  $('#modal-speaker-close').click(function() {
     $('#modal-speaker').css('display', 'none');
     $('#modal-speaker iframe').attr('src', '');
     $('body').removeClass('noscroll');
   });
 
-  window.onclick = function (event) {
+  window.onclick = function(event) {
     var targetId = $(event.target).attr('id');
     switch (targetId) {
       case 'modal-member':
@@ -114,21 +134,21 @@ $(document).ready(function () {
         $('body').removeClass('noscroll');
         break;
     }
-  }
+  };
 
-  var youtubeResize = function () {
+  var youtubeResize = function() {
     var width = $(window).width();
     if (width < 800) {
       $('#ytplayer').attr('width', width);
     }
-  }
+  };
   youtubeResize();
 
-  $(window).resize(function () {
+  $(window).resize(function() {
     youtubeResize();
   });
 
-  $('.menu-wrapper a').click(function () {
+  $('.menu-wrapper a').click(function() {
     $('.navbar__menu').click();
     // const targetSection = $($(this).attr('href'));
     // console.log(targetSection.offset().top);
